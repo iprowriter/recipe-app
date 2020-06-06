402b25e5e07f66cd6f5f86e1f38421a02ec8ae6f
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-06-06 17:06:39 +0200

    Updates

---
+++ precache-manifest.24ee3c7cc4f591f7b625882c0f374754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cc148faea560adc6023154bfdf7cbb",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b5f6ad964164409560ce",
+    "url": "/recipe-app/static/css/main.23ec9a2e.chunk.css"
+  },
+  {
+    "revision": "c51216aa07dd59323a3e",
+    "url": "/recipe-app/static/js/2.e877ef98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.e877ef98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f6ad964164409560ce",
+    "url": "/recipe-app/static/js/main.f8952dc3.chunk.js"
+  },
+  {
+    "revision": "7ff5b27d3f9efd2f4352",
+    "url": "/recipe-app/static/js/runtime-main.96283c75.js"
+  }
+]);
